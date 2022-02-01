60aea11850fcfb69f9df5f5cec889bc3d9f3b58d
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2022-02-01 10:39:52 -0500

    changed spacing

--- AVI/avi_cloud_setup/nsxt_cloud/main.tf
+++ AVI/avi_cloud_setup/nsxt_cloud/main.tf
@@ -76,90 +76,90 @@ resource "nsxt_policy_segment" "segment_dhcp" {
   subnet {
     cidr = var.segment_dhcp_segment_gateway_cidr
     dhcp_ranges = [var.segment_dhcp_segment_dhcp_ip_range]
-    dhcp_v4_config {
-    server_address = var.segment_dhcp_segment_dhcp_ip_address_for_segment
-    lease_time = 36000
-    }
-  }
+     dhcp_v4_config {
+      server_address = var.segment_dhcp_segment_dhcp_ip_address_for_segment
+      lease_time = 36000
+     }
+   }
 }
 
 ## Create NSX-T Credentials for AVI NSX-T Cloud
 resource "avi_cloudconnectoruser" "nsx_t" {
     name = var.avi_nsxt_cred_name
     tenant_ref = "/api/tenant/?name=admin"
-	nsxt_credentials {
-    username = var.nsx_user
-    password = var.nsx_password
-  }
+    nsxt_credentials {
+     username = var.nsx_user
+     password = var.nsx_password
+    }
 }
 
 ## Create vCenter Credentials for AVI NSX-T Cloud
 resource "avi_cloudconnectoruser" "vcenter" {
     name = var.avi_vcenter_cred_name
     tenant_ref = "/api/tenant/?name=admin"
-	vcenter_credentials {
-    username = var.vcenter_user
-    password = var.vcenter_password
-  }
+    vcenter_credentials {
+     username = var.vcenter_user
+     password = var.vcenter_password
+    }
 }
 
 resource "avi_cloud" "nsxt_cloud" {
     name = var.avi_cloud_name_nsx
     tenant_ref = "/api/tenant/?name=admin"
-	vtype = var.avi_cloud_type_nsx
-	obj_name_prefix = var.obj_name_prefix_nsx
-	dhcp_enabled = true
-	dns_provider_ref = avi_ipamdnsproviderprofile.test_dns_nsx.id
-	ipam_provider_ref = avi_ipamdnsproviderprofile.test_ipam_nsx.id
-	nsxt_configuration {
+    vtype = var.avi_cloud_type_nsx
+    obj_name_prefix = var.obj_name_prefix_nsx
+    dhcp_enabled = true
+    dns_provider_ref = avi_ipamdnsproviderprofile.test_dns_nsx.id
+    ipam_provider_ref = avi_ipamdnsproviderprofile.test_ipam_nsx.id
+    nsxt_configuration {
     nsxt_url = var.nsx_address
     nsxt_credentials_ref = avi_cloudconnectoruser.nsx_t.id
-	  management_network_config {
-      transport_zone = data.nsxt_policy_transport_zone.overlay_tz.path
-      tz_type = "OVERLAY"
-      overlay_segment {
-        tier1_lr_id = data.nsxt_policy_tier1_gateway.tier1_gateway.path
-        segment_id  = nsxt_policy_segment.segment_dhcp.path
-      }
+    management_network_config {
+     transport_zone = data.nsxt_policy_transport_zone.overlay_tz.path
+     tz_type = "OVERLAY"
+     overlay_segment {
+      tier1_lr_id = data.nsxt_policy_tier1_gateway.tier1_gateway.path
+      segment_id  = nsxt_policy_segment.segment_dhcp.path
+     }
     }
     data_network_config {
-      transport_zone = data.nsxt_policy_transport_zone.overlay_tz.path
-      tz_type = "OVERLAY"
-      tier1_segment_config {
-        segment_config_mode = "TIER1_SEGMENT_MANUAL"
-        manual {
-          tier1_lrs {
-            tier1_lr_id = data.nsxt_policy_tier1_gateway.tier1_gateway.path
-            segment_id  = nsxt_policy_segment.segment_dhcp.path
-          }
-        }
+     transport_zone = data.nsxt_policy_transport_zone.overlay_tz.path
+     tz_type = "OVERLAY"
+     tier1_segment_config {
+      segment_config_mode = "TIER1_SEGMENT_MANUAL"
+      manual {
+       tier1_lrs {
+        tier1_lr_id = data.nsxt_policy_tier1_gateway.tier1_gateway.path
+        segment_id  = nsxt_policy_segment.segment_dhcp.path
+       }
       }
+     }
     }
-  }
+   }
 }
 
 ## Create NSX ipam profile
 resource "avi_ipamdnsproviderprofile" "test_ipam_nsx" {
-	name	= var.ipam_name_nsx
-	type	= "IPAMDNS_TYPE_INTERNAL"
-	internal_profile {
-		usable_networks {
-			nw_ref = nsxt_policy_segment.segment_dhcp.id
-		}
-	}
+ name = var.ipam_name_nsx
+ type = "IPAMDNS_TYPE_INTERNAL"
+ internal_profile {
+  usable_networks {
+   nw_ref = nsxt_policy_segment.segment_dhcp.id
+  }
+ }
 }
 
 ## Create NSX dns profile
 resource "avi_ipamdnsproviderprofile" "test_dns_nsx" {
-	name	= var.dns_profile_name_nsx
-	type	= "IPAMDNS_TYPE_INTERNAL_DNS"
-	internal_profile {
-		dns_service_domain {
-			domain_name  = var.dns_domain_name
-			pass_through = false
-			record_ttl   = 30
-		}
-	}
+ name = var.dns_profile_name_nsx
+ type = "IPAMDNS_TYPE_INTERNAL_DNS"
+ internal_profile {
+  dns_service_domain {
+   domain_name  = var.dns_domain_name
+   pass_through = false
+   record_ttl   = 30
+  }
+ }
 }
 
 ## Create AVI Content Lib
@@ -175,28 +175,28 @@ resource "avi_vcenterserver" "vcenter" {
 
 ## Create Service Engine Group for NSX-T Cloud
 resource "avi_serviceenginegroup" "nsxt_cloud" {
-    name = var.segroup_nsx_name
-	se_name_prefix = var.se_name_prefix_nsx
-    max_vs_per_se = var.max_vs_per_se
-	min_scaleout_per_vs = var.min_scaleout_per_vs
-	vcpus_per_se = var.vcpus_per_se
-	memory_per_se = var.memory_per_se
-	ha_mode = var.ha_mode
-	algo = var.algo
-	distribute_load_active_standby = var.distribute_load_active_standby_false
-    auto_redistribute_active_standby_load = var.auto_redistribute_active_standby_load
-	se_dp_max_hb_version = var.se_dp_max_hb_version
-	disk_per_se = var.disk_per_se
-	cloud_ref = avi_cloud.nsxt_cloud.id
-	vcenters {
-	 vcenter_ref = avi_vcenterserver.vcenter.id
-     nsxt_datastores {
-      include = true
-      ds_ids = [data.vsphere_datastore.datastore.id]
-     }
-     nsxt_clusters {
-      include = true
-      cluster_ids = [data.vsphere_compute_cluster.cluster.id]
-     }
-	}		
+  name = var.segroup_nsx_name
+  se_name_prefix = var.se_name_prefix_nsx
+  max_vs_per_se = var.max_vs_per_se
+  min_scaleout_per_vs = var.min_scaleout_per_vs
+  vcpus_per_se = var.vcpus_per_se
+  memory_per_se = var.memory_per_se
+  ha_mode = var.ha_mode
+  algo = var.algo
+  distribute_load_active_standby = var.distribute_load_active_standby_false
+  auto_redistribute_active_standby_load = var.auto_redistribute_active_standby_load
+  se_dp_max_hb_version = var.se_dp_max_hb_version
+  disk_per_se = var.disk_per_se
+  cloud_ref = avi_cloud.nsxt_cloud.id
+  vcenters {
+    vcenter_ref = avi_vcenterserver.vcenter.id
+    nsxt_datastores {
+     include = true
+     ds_ids = [data.vsphere_datastore.datastore.id]
+    }
+    nsxt_clusters {
+     include = true
+     cluster_ids = [data.vsphere_compute_cluster.cluster.id]
+    }
+   }		
 }
